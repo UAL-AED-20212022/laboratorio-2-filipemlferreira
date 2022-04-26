57bea342c0fc113ab9cd4515c956898434d8e90e
Filipe Ferreira <30008481@students.ual.pt>
2022-04-26 13:26:39 +0100

    Atualizar

--- controller.py
+++ controller.py
@@ -1,1 +1,17 @@
+from models.LinkedList import LinkedList
 
+def registar_nome_pais_inicio(lista_paises, nome_pais):
+    lista_paises.insert_at_start(nome_pais)
+    return lista_paises
+
+def registar_nome_pais_fim(lista_paises, nome_pais):
+    lista_paises.insert_at_end(nome_pais)
+    return lista_paises
+
+def registar_nome_pais_depois_outro_elemento(lista_paises, nome_pais):
+    lista_paises.insert_after_item(nome_pais)
+    return lista_paises
+
+def registar_nome_pais_antes_outro_elemento(lista_paises, nome_pais):
+    lista_paises.insert_before_item(nome_pais)
+    return lista_paises
--- view.py
+++ view.py
@@ -1,13 +1,55 @@
 import controller
+from models.LinkedList import LinkedList
+
+
+lista_paises = LinkedList()
+
 
 def main():
   
   while True:
+    
+    
     instrucao = input().split(" ")
+
+
     if instrucao[0] == "RPI":
       pais_novo = instrucao[1]
-      
-     
-      
+      RPI(lista_paises, pais_novo)
+
+    if instrucao[0] == "RPF":
+      pais_novo = instrucao[1]
+      RPF(lista_paises, pais_novo)
+
+    if instrucao[0] == "RPDE":
+      pais_novo = instrucao[1]
+      RPDE(lista_paises, pais_novo)
+
+    if instrucao[0] == "RPAE":
+      pais_novo = instrucao[1]
+      RPDE(lista_paises, pais_novo)
 
 
+
+
+
+
+
+
+
+def RPI(lista_paises, nome_pais):
+  controller.registar_nome_pais_inicio(lista_paises, nome_pais)
+  return lista_paises.traverse_list()
+
+
+def RPF(lista_paises, nome_pais):
+  controller.registar_nome_pais_fim(lista_paises, nome_pais)
+  return lista_paises.traverse_list()
+
+def RPDE(lista_paises, nome_pais):
+  controller.registar_nome_pais_depois_outro_elemento(lista_paises, nome_pais)
+  return lista_paises.traverse_list()
+
+def RPAE(lista_paises, nome_pais):
+  controller.registar_nome_pais_antes_outro_elemento(lista_paises,nome_pais)
+  return lista_paises.traverse_list()
