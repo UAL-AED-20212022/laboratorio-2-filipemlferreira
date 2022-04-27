b149eebec5cb3fe1ac8f2fc5b893df4bc25a6a86
Filipe Ferreira <30008481@students.ual.pt>
2022-04-27 11:19:08 +0100

    Quase no fim

--- controller.py
+++ controller.py
@@ -15,3 +15,19 @@ def registar_nome_pais_depois_outro_elemento(lista_paises, nome_pais):
 def registar_nome_pais_antes_outro_elemento(lista_paises, nome_pais):
     lista_paises.insert_before_item(nome_pais)
     return lista_paises
+
+def registar_nome_pais_num_determinado_index(lista_paises, nome_pais):
+    lista_paises.insert_at_index(nome_pais)
+    return lista_paises
+
+def verificar_numero_elementos(lista_paises):
+    return lista_paises.get_count()
+
+def verificar_nome_pais(lista_paises, nome_pais):
+    lista_paises.search_item(nome_pais)
+    return nome_pais
+
+def elimina_nome_inicio(lista_paises):
+    pais_numero_um = lista_paises.start_node.item
+    lista_paises.delete_at_start()
+    return pais_numero_um
--- view.py
+++ view.py
@@ -28,6 +28,29 @@ def main():
     if instrucao[0] == "RPAE":
       pais_novo = instrucao[1]
       RPDE(lista_paises, pais_novo)
+    
+    if instrucao[0] == "RPII":
+      pais_novo = instrucao[1]
+      RPII(lista_paises, pais_novo)
+
+    if instrucao[0] == "VNE":
+      VNE(lista_paises)
+      print(f"O número de elementos são {len(lista_paises)}")
+    
+    if instrucao[0] == "VP":
+      pais_novo = instrucao[1]
+      if VP(lista_paises) == True:
+        print(f"O país {pais_novo} encontra-se na lista.")
+      else:
+          print(f"O país {pais_novo} não se encontra na lista.")
+    
+    if instrucao[0] == "EPE":
+      EPE(lista_paises)
+      print(f"O país {EPE(lista_paises)} foi eliminado da lista. ")
+    
+    if instrucao[0] == "EUE":
+      EUE(lista_paises)
+      print(f"O país {EUE(lista_paises)} foi eliminado da lista. ")
 
 
 
@@ -53,3 +76,25 @@ def RPDE(lista_paises, nome_pais):
 def RPAE(lista_paises, nome_pais):
   controller.registar_nome_pais_antes_outro_elemento(lista_paises,nome_pais)
   return lista_paises.traverse_list()
+
+def RPII(lista_paises, nome_pais):
+  controller.registar_nome_pais_num_determinado_index(lista_paises, nome_pais)
+  return lista_paises.traverse_list()
+
+def VNE(lista_paises):
+  return controller.verificar_numero_elementos(lista_paises)
+
+def VP(lista_paises, pais):
+  if controller.verificar_nome_pais(lista_paises, pais) == True:
+    return True
+  else:
+    return False
+
+def EPE(lista_paises):  
+  return controller.elimina_nome_inicio(lista_paises)
+
+def EUE(lista_paises):
+  return controller.elimina_nome_fim(lista_paises)
+  
+  
+  
